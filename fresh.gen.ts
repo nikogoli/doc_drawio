d807d9e3f309085ccbfc4fc5780c55231f0c1435
nikogoli <k_m_naoto@yahoo.co.jp>
2024-02-04 21:08:17 +0900

    Update fresh.gen.ts

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -8,9 +8,10 @@ import * as $2 from "./routes/index.tsx";
 import * as $3 from "./routes/page/[name].tsx";
 import * as $$0 from "./islands/AttrPart.tsx";
 import * as $$1 from "./islands/AttrbutesPart.tsx";
-import * as $$2 from "./islands/DocPartElems.tsx";
-import * as $$3 from "./islands/ListItemElem.tsx";
-import * as $$4 from "./islands/Page.tsx";
+import * as $$2 from "./islands/ChangeClassButton.tsx";
+import * as $$3 from "./islands/DocPartElems.tsx";
+import * as $$4 from "./islands/ListItemElem.tsx";
+import * as $$5 from "./islands/Page.tsx";
 
 const manifest = {
   routes: {
@@ -22,9 +23,10 @@ const manifest = {
   islands: {
     "./islands/AttrPart.tsx": $$0,
     "./islands/AttrbutesPart.tsx": $$1,
-    "./islands/DocPartElems.tsx": $$2,
-    "./islands/ListItemElem.tsx": $$3,
-    "./islands/Page.tsx": $$4,
+    "./islands/ChangeClassButton.tsx": $$2,
+    "./islands/DocPartElems.tsx": $$3,
+    "./islands/ListItemElem.tsx": $$4,
+    "./islands/Page.tsx": $$5,
   },
   baseUrl: import.meta.url,
 };
